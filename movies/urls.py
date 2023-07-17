bc4112bf4011511d726cbe1132f4a7f027367214
Chieto08 <chieto.chiedu@gmail.com>
2023-07-17 20:37:05 +0100

    CRUD Operations added to movies app without edit operation yet

--- movies/urls.py
+++ movies/urls.py
@@ -21,6 +21,9 @@ from movies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies', views.movies),
-    path('', views.home)
+    path('movies/', views.movies),
+    path('', views.home),
+    path('movies/<int:id>', views.movie_detail),
+    path('movies/add/', views.add),
+    path('movies/delete/<int:id>', views.delete)
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,31 +1,39 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-
-data = {
-    'movies': [
-        {
-            'id': 1,
-            'title': 'Witcher',
-            'year': 2023
-        },
-        {
-            'id': 2,
-            'title': 'Ginny and Georgia',
-            'year': 2022
-        },
-        {
-            'id': 3,
-            'title': 'Barbie',
-            'year': 2023
-        },
-    ]
-}
+from .models import Movie
 
 
 def movies(request):
-    return render(request, 'movies/movies.html', data)
+    all_movies = Movie.objects.all()
+    return render(request, 'movies/movies.html', {'movies': all_movies})
 
 
 def home(request):
     return HttpResponse("Hello homepage")
+
+
+def movie_detail(request, id):
+    data = Movie.objects.get(pk=id)
+    return render(request, 'movies/detail.html', {'movie': data})
+
+
+def add(request):
+    title = request.POST.get('title')
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+
+    return render(request, 'movies/add.html')
+
+
+def delete(request, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404("Movie does not exist")
+    movie.delete()
+
+    return HttpResponseRedirect("/movies")
